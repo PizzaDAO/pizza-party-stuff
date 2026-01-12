3525be978b81f024b516854db833770d37c6b958
snackman <snax.ynot@gmail.com>
2026-01-12 16:43:45 -0500

    new items

---
+++ src/data/supplies.ts
@@ -0,0 +1,172 @@
+import { Supply } from '@/components/SuppliesTable';
+
+export const pizzaPartySupplies: Supply[] = [
+  {
+    id: 1,
+    item: "Red and White Tablecloth",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 2,
+    item: "Pizza Dog Toy",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: "chewy.com link"
+  },
+  {
+    id: 3,
+    item: "Pizza Pocket",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 4,
+    item: "Pizza Earrings",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: "temu.com link"
+  },
+  {
+    id: 5,
+    item: "Pizza Dog Costume",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 6,
+    item: "Pizza Necklace",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 7,
+    item: "Pizza Napkin",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: "walmart.com link"
+  },
+  {
+    id: 8,
+    item: "Pizza Floatie",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: "swimoutlet.com link"
+  },
+  {
+    id: 9,
+    item: "Pizza Cocktail Umbrella",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 10,
+    item: "Pizza Plates",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: "partycity.com link"
+  },
+  {
+    id: 11,
+    item: "Pizza Flip Flop",
+    rating: 5,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 12,
+    item: "Pizza Jibbitz",
+    rating: 3,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 13,
+    item: "Pizza Hoodie",
+    rating: 3,
+    cost: "",
+    link: "",
+    notes: "thepizzapocket.com link"
+  },
+  {
+    id: 14,
+    item: "Pizza Toy",
+    rating: 3,
+    cost: "",
+    link: "",
+    notes: "amazon.com link"
+  },
+  {
+    id: 15,
+    item: "Pizza Hat",
+    rating: 3,
+    cost: "",
+    link: "",
+    notes: "windycitynovelties.com link"
+  },
+  {
+    id: 16,
+    item: "Pizza Thong",
+    rating: 3,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 17,
+    item: "Bad Pizza Float",
+    rating: 1,
+    cost: "",
+    link: "",
+    notes: "shopleisuredepot.com link"
+  },
+  {
+    id: 18,
+    item: "Pizza Chip Clips",
+    rating: 1,
+    cost: "",
+    link: "",
+    notes: "walmart.com link"
+  },
+  {
+    id: 19,
+    item: "Pizza Plushy",
+    rating: 1,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 20,
+    item: "Detroit Style Pizza Pot Holder",
+    rating: 1,
+    cost: "",
+    link: "",
+    notes: ""
+  },
+  {
+    id: 21,
+    item: "Pizza Coaster",
+    rating: 1,
+    cost: "",
+    link: "",
+    notes: ""
+  }
+];
